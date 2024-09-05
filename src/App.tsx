ca2fdf0041513d70f8fc3e0949bc604bdc479e40
Dido Arellano <didoarellano@gmail.com>
2024-09-05 22:11:18 +0930

    Set up Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import { useState, useEffect } from "react";
+import { onSnapshot, addDoc, collection } from "firebase/firestore";
+import { db } from "./services/firebase";
+
+type Bleh = {
+  id: string;
+  timestamp: string;
+};
+
+async function addBleh() {
+  try {
+    await addDoc(collection(db, "bleh"), {
+      timestamp: Date.now(),
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 function App() {
+  const [blehs, setBlehs] = useState<Bleh[] | []>([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "bleh"), (snapshot) => {
+      let docs: Bleh[] = [];
+      snapshot.forEach((doc) => {
+        let _bleh: Bleh = {
+          id: doc.id,
+          timestamp: doc.data().timestamp,
+        };
+        docs.push(_bleh);
+      });
+      setBlehs(docs);
+    });
+
+    return unsub;
+  }, []);
+
   return (
     <>
-      <h1>Hello Haderach</h1>
+      <button onClick={addBleh}>bleh</button>
+      <ol>
+        {blehs.map((bleh) => {
+          return <li key={bleh.id}>{bleh.timestamp}</li>;
+        })}
+      </ol>
     </>
   );
 }
