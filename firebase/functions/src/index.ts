685e4e9d4a8d6f314feef3511fc10a7f921346a5
Dido Arellano <didoarellano@gmail.com>
2024-10-10 10:02:10 +1100

    Add createGame cloud function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -8,32 +8,67 @@
  */
 
 import * as admin from "firebase-admin";
-import { onCall } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { HttpsError, onCall } from "firebase-functions/v2/https";
 
 admin.initializeApp();
 const db = admin.firestore();
 
-export const ping = onCall(async () => {
-  const counterRef = db.collection("hits").doc("hitCount");
-  try {
-    const counterDoc = await counterRef.get();
-    let newCount;
-    if (counterDoc.exists) {
-      const currentCount = counterDoc.data()?.count || 0;
-      newCount = currentCount + 1;
-      await counterRef.update({ count: newCount });
-    } else {
-      await counterRef.set({ count: 1 });
-      newCount = 1;
-    }
-    return {
-      hits: newCount,
-    };
-  } catch (error) {
-    logger.error("Error incrementing hit counter:", error);
-    return {
-      error,
-    };
+import { customAlphabet } from "nanoid";
+
+const generatePIN = customAlphabet("0123456789", 6);
+
+export const createGame = onCall(async (request) => {
+  const quizID = request.data;
+
+  const quizDoc = await db.collection("quizzes").doc(quizID).get();
+
+  if (!quizDoc.exists) {
+    logger.error(`Quiz with ID ${quizID} not found.`);
+    throw new HttpsError("not-found", `Quiz with ID ${quizID} does not exist.`);
   }
+
+  const quizData = quizDoc.data();
+
+  let activeGameSnap = await db
+    .collection("activeGames")
+    .where("quizID", "==", quizID)
+    .get();
+
+  let activeGame;
+
+  if (!activeGameSnap.empty) {
+    const activeGameDoc = activeGameSnap.docs[0];
+    activeGame = activeGameDoc.data();
+    activeGame.id = activeGameDoc.id;
+    activeGame.quizData = quizData;
+    return activeGame;
+  }
+
+  let pin;
+  let pinExists = true;
+  while (pinExists) {
+    pin = generatePIN();
+
+    const pinQuery = await db
+      .collection("activeGames")
+      .where("pin", "==", pin)
+      .get();
+
+    pinExists = !pinQuery.empty;
+  }
+
+  activeGame = {
+    pin,
+    quizID,
+    teacherID: quizData.teacherID,
+    currentQuestionIndex: 0,
+  };
+
+  db.collection("activeGames").add(activeGame);
+
+  return {
+    ...activeGame,
+    quizData,
+  };
 });
