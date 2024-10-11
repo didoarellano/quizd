53c5f6cdf929fcd45e29a71a8512490e98729d3b
Dido Arellano <didoarellano@gmail.com>
2024-10-11 17:23:17 +1100

    Remove temp next button in <Quiz/>

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,34 +1,18 @@
-import { useState } from "react";
 import { type Quiz as QuizType } from "../services/quiz";
 import { QuestionText } from "./QuestionText";
 import { Option } from "./Option";
 
 type QuizProps = {
   quiz: QuizType;
+  questionIndex: number;
 };
 
-export function Quiz({ quiz }: QuizProps) {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const currentQuestion = quiz.questions[currentQuestionIndex];
+export function Quiz({ quiz, questionIndex }: QuizProps) {
+  const currentQuestion = quiz.questions[questionIndex];
 
   return (
     <>
       <h2>{quiz.title}</h2>
-
-      {/* TEMPORARY */}
-      {!(quiz.questions.length - 1 === currentQuestionIndex) && (
-        <button
-          onClick={() => {
-            setCurrentQuestionIndex((prevIndex) => {
-              return prevIndex + 1;
-            });
-          }}
-        >
-          Next
-        </button>
-      )}
-      {/* TEMPORARY */}
-
       <div>
         <QuestionText
           heading={currentQuestion.heading}
