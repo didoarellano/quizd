73e88003d817b1e454bae997556417bbde8ed696
Dido Arellano <didoarellano@gmail.com>
2024-10-13 13:24:59 +1100

    Use react-query in <QuizEditor/>

--- src/pages/QuizEditor.tsx
+++ src/pages/QuizEditor.tsx
@@ -1,13 +1,23 @@
-import { useEffect, useRef, useState } from "react";
-import type { DocumentData, DocumentSnapshot } from "firebase/firestore";
-import { Link, useLocation, useRouter, type RouteComponentProps } from "wouter";
-
-import { generateID, getQuiz, saveNewQuiz, updateQuiz } from "../services/quiz";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  Link,
+  Redirect,
+  useLocation,
+  useRouter,
+  type RouteComponentProps,
+} from "wouter";
+import { MarkdownEditor } from "../components/MarkdownEditor";
 import { QuizDeleteButton } from "../components/QuizDeleteButton";
 import { QuizPreview } from "../components/QuizPreview";
 import { useAuth } from "../contexts/AuthContext";
 import { Teacher, UserRoles } from "../services/auth";
-import { MarkdownEditor } from "../components/MarkdownEditor";
+import {
+  deleteQuizByID,
+  generateID,
+  getQuiz,
+  saveNewQuiz,
+  updateQuiz,
+} from "../services/quiz";
 import { parseQuiz } from "../utils/markdown";
 
 type QuizEditorProps = {
@@ -15,69 +25,80 @@ type QuizEditorProps = {
 } & Partial<RouteComponentProps>;
 
 export function QuizEditor({ quizID }: QuizEditorProps) {
-  const quizSnap = useRef<DocumentSnapshot | null>(null);
-  const [quizData, setQuizData] = useState<DocumentData | null>(null);
-  const [isLoadingQuiz, setIsLoadingQuiz] = useState(true);
   const [location, setLocation] = useLocation();
   const { base } = useRouter();
   const { user } = useAuth();
 
-  useEffect(() => {
-    if (!user?.id || user?.role !== UserRoles.Teacher) return;
-    if (!quizID) return setIsLoadingQuiz(false);
-    setIsLoadingQuiz(true);
-    getQuiz(quizID, user.id)
-      .then(({ docSnap, quizData: data }) => {
-        quizSnap.current = docSnap;
-        setQuizData(data);
-      })
-      .catch((e) => {
-        // TODO: Flash message
-        setLocation(`~${base}`);
-      })
-      .finally(() => setIsLoadingQuiz(false));
-  }, []);
+  const queryClient = useQueryClient();
+  const { isPending, isError, error, data } = useQuery({
+    queryKey: ["quizzes", quizID],
+    queryFn: () => getQuiz(quizID, user.id),
+    enabled: !!(quizID && user?.role === UserRoles.Teacher),
+    // TODO: If 404 redirect immediately, don't retry
+    // retry: (count, error) => {}
+  });
 
-  async function handleSave(mdText: string) {
-    let quiz = parseQuiz(generateID, mdText);
-    quiz._rawMD = mdText;
-    if (!quizID) {
-      const quizDocRef = await saveNewQuiz(user as Teacher, quiz);
-      const loc = `~${base}/${quizDocRef.id}`;
-      setLocation(loc, { replace: true });
-      return;
-    }
-    if (quizSnap?.current?.ref) {
-      updateQuiz(quizSnap.current.ref, quiz);
-      setQuizData(quiz);
-    }
+  const { mutate: deleteQuiz } = useMutation({
+    mutationFn: () => deleteQuizByID(quizID),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["quizzes", quizID] });
+      setLocation("/", { replace: true });
+    },
+  });
+
+  const { mutate: createNewQuiz } = useMutation({
+    mutationFn: (mdText: string) => {
+      let quiz = parseQuiz(generateID, mdText);
+      quiz._rawMD = mdText;
+      return saveNewQuiz(user as Teacher, quiz);
+    },
+    onSuccess: (quizRef) => {
+      queryClient.invalidateQueries({ queryKey: ["quizzes"] });
+      setLocation(`~${base}/${quizRef.id}`, { replace: true });
+    },
+  });
+
+  const { mutate: saveQuiz } = useMutation({
+    mutationFn: (mdText: string) => {
+      let quiz = parseQuiz(generateID, mdText);
+      quiz._rawMD = mdText;
+      return updateQuiz(data.quizSnap.ref, quiz);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["quizzes", quizID] });
+    },
+  });
+
+  if (!quizID) {
+    return (
+      <>
+        <h1>Create Quiz</h1>
+        <MarkdownEditor initialMDText="" handleSave={createNewQuiz} />
+      </>
+    );
   }
 
-  async function handleDelete() {
-    setLocation("/", { replace: true });
+  if (isError) {
+    // TODO: Flash message
+    console.error(error);
+    return <Redirect to={`~${base}`} />;
   }
 
   return (
     <>
-      <h1>
-        {!quizID ? "Create Quiz" : `Editing Quiz: ${quizData?.title ?? ""}`}
-      </h1>
-
-      {quizID && <Link href={`${location}/host`}>Host Game</Link>}
-
-      {isLoadingQuiz ? (
-        <p>...</p>
+      <h1>Editing Quiz: {data?.quizData?.title ?? ""}</h1>
+      <Link href={`${location}/host`}>Host Game</Link>
+      {isPending ? (
+        <p>loading...</p>
       ) : (
         <>
-          <QuizDeleteButton quizID={quizID} onDelete={handleDelete} />
+          <QuizDeleteButton onDeleteClick={deleteQuiz} />
           <MarkdownEditor
-            initialMDText={quizData?._rawMD ?? ""}
-            handleSave={handleSave}
+            initialMDText={data.quizData._rawMD}
+            handleSave={saveQuiz}
           />
-
           <hr />
-
-          {quizData && <QuizPreview quiz={quizData} />}
+          <QuizPreview quiz={data.quizData} />
         </>
       )}
     </>
