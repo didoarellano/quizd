3db2601039327105e5a3e53b186b0ac90e9c7d03
Dido Arellano <didoarellano@gmail.com>
2024-10-25 14:31:19 +1030

    Update game status on game start

--- client/src/pages/HostLobby.tsx
+++ client/src/pages/HostLobby.tsx
@@ -1,8 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import { doc, updateDoc } from "firebase/firestore";
 import { Link } from "wouter";
+import { GameStatus } from "../../../shared/game.types";
+import { db } from "../services/firebase";
 import { useGame } from "../utils/useGame";
+
+const startButtonText = {
+  [GameStatus.PENDING]: "Start Game",
+  [GameStatus.ONGOING]: "Continue Game",
+  [GameStatus.COMPLETED]: "View Results",
+};
 
 export function HostLobby({ quizID }: { quizID: string }) {
   const { data: game } = useGame(quizID);
+  const { mutate: updateGameStatus } = useMutation({
+    mutationFn: () => {
+      if (game && game.status === GameStatus.PENDING) {
+        const gameRef = doc(db, "games", game.id);
+        return updateDoc(gameRef, { status: GameStatus.ONGOING });
+      }
+      return Promise.resolve();
+    },
+  });
+
   const players = game?.players;
   const q = game?.currentQuestionIndex ?? 0;
 
@@ -15,8 +35,8 @@ export function HostLobby({ quizID }: { quizID: string }) {
       <p>{game?.quizData.description}</p>
       <p>Go to: {window.location.href.replace(/host\/.*/i, "play")}</p>
       <p>Enter PIN: {game?.pin}</p>
-      <Link href={`/play?q=${q}`}>
-        {q === 0 ? "Start Game" : "Continue Game"}
+      <Link href={`/play?q=${q}`} onClick={() => updateGameStatus()}>
+        {startButtonText[game?.status]}
       </Link>
       <div>
         <h3>Players</h3>
