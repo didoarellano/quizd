3508e84ebd5b18139a4390da2f0189d8e48cdf2a
Dido Arellano <didoarellano@gmail.com>
2024-10-26 13:41:12 +1030

    Update currentQuestionIndex on query string change

--- client/src/pages/GameQuestion.tsx
+++ client/src/pages/GameQuestion.tsx
@@ -1,22 +1,44 @@
+import { useMutation } from "@tanstack/react-query";
+import { doc, updateDoc } from "firebase/firestore";
+import { useEffect } from "react";
 import { Link, Redirect, useSearch } from "wouter";
 import { Option } from "../components/Option";
 import { QuestionText } from "../components/QuestionText";
+import { db } from "../services/firebase";
 import { useGame } from "../utils/useGame";
 
 export function GameQuestion({ quizID }: { quizID: string }) {
   const { data: game } = useGame(quizID);
+  const { mutate: updateCurrentQuestionIndex } = useMutation({
+    mutationFn: (currentQuestionIndex: number) => {
+      if (!game) return Promise.resolve();
+      const gameRef = doc(db, "games", game.id);
+      return updateDoc(gameRef, { currentQuestionIndex });
+    },
+  });
   const searchParams = useSearch();
+  const currentIndex = Number(
+    Object.fromEntries(new URLSearchParams(searchParams))?.q,
+  );
+
+  useEffect(() => {
+    updateCurrentQuestionIndex(currentIndex);
+  }, [currentIndex, updateCurrentQuestionIndex]);
 
   if (!game || !searchParams) return <Redirect to={`/`} />;
 
-  const q = Number(Object.fromEntries(new URLSearchParams(searchParams))?.q);
-  const question = game.quizData.questions[q];
+  const questions = game.quizData.questions;
+  const question = questions && questions[currentIndex];
+  const nextIndex = currentIndex + 1 < questions.length && currentIndex + 1;
 
   return (
     <>
       <h1>Current Question</h1>
       <Link href="/">Host Lobby</Link>
       <Link href="/results">Results</Link>
+
+      <br />
+      {nextIndex && <Link href={`/play?q=${nextIndex}`}>Next Question</Link>}
 
       <article>
         <QuestionText heading={question.heading} body={question.body} />
