60633c6cb913a52efd5e2771f4289b1bc3323603
Dido Arellano <didoarellano@gmail.com>
2024-10-27 13:46:56 +1030

    Add useGame hook for players

--- client/src/pages/PlayerLobby.tsx
+++ client/src/pages/PlayerLobby.tsx
@@ -1,28 +1,16 @@
-import { doc, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
 import { Redirect, useParams } from "wouter";
-import { GameStatus, StoredGame } from "../../../shared/game.types";
+import { GameStatus } from "../../../shared/game.types";
 import { useAuth } from "../contexts/AuthContext";
-import { db } from "../services/firebase";
+import { useGameAsPlayer } from "../utils/useGame";
 
 export function PlayerLobby() {
-  const { gameID } = useParams();
   const { user } = useAuth();
-  const [game, setGame] = useState<StoredGame | null>(null);
+  const { pin } = useParams();
+  const { data } = useGameAsPlayer(pin);
 
-  useEffect(() => {
-    if (!gameID) return;
-    const gameRef = doc(db, "games", gameID);
-    const unsubscribe = onSnapshot(gameRef, async (snapshot) => {
-      const gameData = snapshot.data();
-      if (gameData) setGame(gameData as StoredGame);
-    });
-    return unsubscribe;
-  }, [gameID]);
+  if (!pin) return <Redirect to="/" />;
 
-  if (!gameID) return <Redirect to="/" />;
-
-  if (game?.status === GameStatus.PENDING) {
+  if (data?.game.status === GameStatus.PENDING) {
     return (
       <>
         <p>Welcome {user?.displayName}!</p>
@@ -31,8 +19,8 @@ export function PlayerLobby() {
     );
   }
 
-  if (game?.status === GameStatus.ONGOING) {
-    return <p>Current Question: {game.currentQuestionIndex}</p>;
+  if (data?.game?.status === GameStatus.ONGOING) {
+    return <p>Current Question: {data.game.currentQuestionIndex}</p>;
   }
 
   return <p>...</p>;
--- client/src/utils/useGame.ts
+++ client/src/utils/useGame.ts
@@ -1,9 +1,11 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { updateProfile, User } from "firebase/auth";
 import { doc, onSnapshot } from "firebase/firestore";
 import { httpsCallable } from "firebase/functions";
 import { useEffect } from "react";
 import type { ReturnedGame } from "../../../shared/game.types";
-import { db, functions } from "../services/firebase";
+import { signinAnonymously } from "../services/auth";
+import { auth, db, functions } from "../services/firebase";
 
 const getOrCreateGame = httpsCallable<string, ReturnedGame>(
   functions,
@@ -32,6 +34,48 @@ export function useGame(quizID: string) {
     });
     return unsubscribe;
   }, [queryClient, quizID, queryRes]);
+
+  return queryRes;
+}
+
+const joinGame = httpsCallable(functions, "joinGame");
+
+export function useGameAsPlayer(pin: string) {
+  const queryRes = useQuery({
+    queryKey: ["games", pin],
+    queryFn: async () => {
+      const { data } = await joinGame(pin);
+      const user = auth.currentUser as User;
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+      user.reload();
+      return data;
+    },
+    enabled: !!auth.currentUser,
+  });
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!auth.currentUser) signinAnonymously();
+  }, []);
+
+  useEffect(() => {
+    const gameID = queryRes?.data?.game.id;
+    if (!gameID) return;
+    const gameRef = doc(db, "games", gameID);
+    const unsubscribe = onSnapshot(gameRef, async (snapshot) => {
+      const gameData = snapshot.data();
+      if (gameData) {
+        gameData.id = gameID;
+        queryClient.setQueryData(["games", pin], {
+          ...queryRes.data,
+          game: gameData,
+        });
+      }
+    });
+    return unsubscribe;
+  }, [pin, queryClient, queryRes]);
 
   return queryRes;
 }
