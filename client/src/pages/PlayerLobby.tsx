c432eb2ddd72626bd5ef4710af0975a0ab5c3e35
Dido Arellano <didoarellano@gmail.com>
2024-10-28 11:05:54 +1030

    Display current question

--- client/src/pages/PlayerLobby.tsx
+++ client/src/pages/PlayerLobby.tsx
@@ -1,5 +1,7 @@
 import { Redirect, useParams } from "wouter";
 import { GameStatus } from "../../../shared/game.types";
+import { Option } from "../components/Option";
+import { QuestionText } from "../components/QuestionText";
 import { useAuth } from "../contexts/AuthContext";
 import { useGameAsPlayer } from "../utils/useGame";
 
@@ -10,7 +12,12 @@ export function PlayerLobby() {
 
   if (!pin) return <Redirect to="/" />;
 
-  if (data?.game.status === GameStatus.PENDING) {
+  if (!data) {
+    // TODO redirect?
+    return <p>Game not found.</p>;
+  }
+
+  if (data.game.status === GameStatus.PENDING) {
     return (
       <>
         <p>Welcome {user?.displayName}!</p>
@@ -19,8 +26,18 @@ export function PlayerLobby() {
     );
   }
 
-  if (data?.game?.status === GameStatus.ONGOING) {
-    return <p>Current Question: {data.game.currentQuestionIndex}</p>;
+  if (data.game.status === GameStatus.ONGOING) {
+    const question = data.quiz.questions[data.game.currentQuestionIndex];
+    return (
+      <article>
+        <QuestionText heading={question.heading} body={question.body} />
+        <div>
+          {question.options.map((option) => (
+            <Option key={option.id} text={option.text} />
+          ))}
+        </div>
+      </article>
+    );
   }
 
   return <p>...</p>;
