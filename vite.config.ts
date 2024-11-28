6d4a6a6f56a23f86989748dccfdb153a879f5560
Dido Arellano <didoarellano@gmail.com>
2024-11-28 22:58:44 +1030

    Set up react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig((configEnv) => {
   const env = loadEnv(configEnv.mode, process.cwd(), "");
@@ -9,6 +10,9 @@ export default defineConfig((configEnv) => {
     base: env.VITE_BASE_URL,
     test: {
       environment: "jsdom",
+      globals: true,
+      exclude: [...configDefaults.exclude, "**/firebase/functions/lib/**"],
+      setupFiles: "./vitest.setup.ts",
     },
   };
 });
